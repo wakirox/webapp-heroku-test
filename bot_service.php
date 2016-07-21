3471ea5a393b8c67e7867fae2f05abc5769277d5
Marius Gabriel Magadan <wakirox@gmail.com>
2016-07-21 15:46:14 +0200

    added automation

--- bot_service.php
+++ bot_service.php
@@ -3,7 +3,7 @@
 $botToken = "209848654:AAGkvCOZ2DARAav_25W-gpNzGWQtluxjU_Q";
 $webSite = "https://api.telegram.org/bot".$botToken;
 
-$update = file_get_contents($webSite."/getupdates");
+$update = file_get_contents("php://input");
 
 $updateArray = json_decode($update,true);
 
